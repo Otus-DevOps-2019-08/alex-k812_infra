d2a7e5db864a030f2c7cdabb318098194465af41
Alexandr Kopylov <akspb812@gmail.com>
2019-09-25 21:39:04 +0300

    Fixed command error in startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -5,8 +5,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
 sudo apt update
 sudo apt install -y mongodb-org
-sudo systemctl start mongodb
-sudo systemctl enable mongodb
+sudo systemctl start mongod
+sudo systemctl enable mongod
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
